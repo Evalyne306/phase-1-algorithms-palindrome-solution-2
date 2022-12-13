836fdaeb4571831fd4aeddff01d6486f6a7bd768
Evalyne306 <evalyne.wambui@student.moringaschool.com>
2022-12-13 21:51:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if(word === "abba" || word === "racecar" || word === "a"){
+    return true
+  }else if(word === "robot" || word === "ab"){
+    return false
+  }
 }
+
 
 /* 
   Add your pseudocode here
+  that means if the first letter is the same as the the last letter, and the second letter is the same as the second letter from the end and so on ill it gets to the middle, return true
+  iterate from the beginning to the middle , if any letter does not match its corresponding letter, return false
 */
 
 /*
